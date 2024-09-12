58c82c96193abdb2c8cc52ccf044d18343ed2bf9
Hasan Kara <dev.hasan.kara@gmail.com>
2024-09-13 01:43:47 +0300

    update sitemap

--- src/restoreplus-sdk/v2/sitemap/methods.ts
+++ src/restoreplus-sdk/v2/sitemap/methods.ts
@@ -1,10 +1,20 @@
-import { serverFetcher } from '@/lib/server-fetcher'
+import 'server-only'
 
 export const sitemapMethods = {
   getSitemap: async () => {
-    const { data } = await serverFetcher('/v2/sitemap', {
+    const res = await fetch('https://data.restoreplus.store/api/v2/sitemap', {
       method: 'GET',
+      headers: {
+        ['x-api-key']: process.env.REMOTE_X_API_KEY
+          ? process.env.REMOTE_X_API_KEY
+          : process.env.X_API_KEY || '',
+        ['x-api-key-name']: process.env.REMOTE_X_API_KEY
+          ? process.env.REMOTE_X_API_KEY
+          : process.env.X_API_KEY || '',
+      },
     })
+
+    const data = await res.json()
 
     return data
   },
