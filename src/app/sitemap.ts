4cbc0ddd5a8a8f4197ec8db09a1a97c261a57f68
Hasan Kara <dev.hasan.kara@gmail.com>
2024-09-13 04:07:05 +0300

    make 3600 seconds generation of sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,40 @@
-import { sdk } from '@/restoreplus-sdk'
 import type { MetadataRoute } from 'next'
 
-export const revalidate = 30
+export const revalidate = 3600
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const sitemap = await sdk.sitemap.getSitemap()
+  // const sitemap = await sdk.sitemap.getSitemap()
 
-  return sitemap
+  return [
+    {
+      url: 'https://acme.com',
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          es: 'https://acme.com/es',
+          de: 'https://acme.com/de',
+        },
+      },
+    },
+    {
+      url: 'https://acme.com/about',
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          es: 'https://acme.com/es/about',
+          de: 'https://acme.com/de/about',
+        },
+      },
+    },
+    {
+      url: 'https://acme.com/blog',
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          es: 'https://acme.com/es/blog',
+          de: 'https://acme.com/de/blog',
+        },
+      },
+    },
+  ]
 }
